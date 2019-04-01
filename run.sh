d5aeec638060c61e495d0d29636f0170575abed1
Shuyu Chen <shuyuchen@ibm.com>
2019-04-01 15:54:11 -0500

    updated param to have quotes

--- run.sh
+++ run.sh
@@ -25,20 +25,20 @@ ibmcloud login --apikey $IBM_CLOUD_API_KEY --no-region
 
 #
 # Upload a build record for this build, It is assumed that the build was successful
-ibmcloud doi publishbuildrecord --logicalappname=$MY_APP_NAME --buildnumber=$MY_BUILD_NUMBER --branch=master --repositoryurl=https://github.com/open-toolchain/dra-toolchain-demo --commitid=2b5fd14 --status=pass
+ibmcloud doi publishbuildrecord --logicalappname="$MY_APP_NAME" --buildnumber="$MY_BUILD_NUMBER" --branch=master --repositoryurl=https://github.com/open-toolchain/dra-toolchain-demo --commitid=2b5fd14 --status=pass
 
 #
 # Upload unittest test record for the build
-ibmcloud doi publishtestrecord --logicalappname=$MY_APP_NAME --buildnumber=$MY_BUILD_NUMBER --filelocation=mocha_pass.json --type=unittest
+ibmcloud doi publishtestrecord --logicalappname="$MY_APP_NAME" --buildnumber="$MY_BUILD_NUMBER" --filelocation=mocha_pass.json --type=unittest
 
 #
 # Upload CodeCoverge test record for the build
-ibmcloud doi publishtestrecord --logicalappname=$MY_APP_NAME --buildnumber=$MY_BUILD_NUMBER --filelocation=istanbul_pass.json --type=code
+ibmcloud doi publishtestrecord --logicalappname="$MY_APP_NAME" --buildnumber="$MY_BUILD_NUMBER" --filelocation=istanbul_pass.json --type=code
 
 #
 # Invoke a DevOps Insights gate to evaluated a policy based on uploaded data
-ibmcloud doi evaluategate --logicalappname=$MY_APP_NAME --buildnumber=$MY_BUILD_NUMBER --policy=FirstPolicy
+ibmcloud doi evaluategate --logicalappname="$MY_APP_NAME" --buildnumber="$MY_BUILD_NUMBER" --policy=FirstPolicy
 
 #
 # Upload a deployment record, It is assumed that the deployment was successful
-ibmcloud doi publishdeployrecord --logicalappname=$MY_APP_NAME --buildnumber=$MY_BUILD_NUMBER --joburl="cloud.ibm.com" --env=dev --status=pass
+ibmcloud doi publishdeployrecord --logicalappname="$MY_APP_NAME" --buildnumber="$MY_BUILD_NUMBER" --env=dev --status=pass
